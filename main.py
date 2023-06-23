f2da7d6d2d25625717a4696c247dfec11b7c5a55
muhi <muhi@LAPTOP-7MSLSDP9>
2023-06-24 06:25:54 +0900

    一応APIは叩けるようになった

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import gen_url
-import requests,re,json,time,uuid
+import requests,re,json,time,sys
 
 def get_session_token():
 	login_url,session_token_code_verifier = gen_url.getNSOLogin()
@@ -35,13 +35,17 @@ def get_access_token(session_token):
 	response = requests.post(url, headers=headers, json=body)
 	return json.loads(response.text)["access_token"], json.loads(response.text)["id_token"]
 
-def get_f(access_token):
+def get_f(id_token,hash_method):
 	url = "https://api.imink.app/f"
 	body = {
-		"token": access_token,
-		"hash_method": 1
+		"token": id_token,
+		"hash_method": hash_method
 	}
 	response = requests.post(url, json=body)
+	if response.status_code // 100 != 2:
+		print("error occur in generating f")
+		print("check \"https://status.imink.app/\"")
+		sys.exit()
 	return {
 		"f" : json.loads(response.text)["f"],
 		"timestmp" :  json.loads(response.text)["timestamp"],
@@ -59,10 +63,10 @@ def get_me(access_token):
 	user_info = json.loads(response.text)
 	return user_info["language"],user_info["country"],user_info["birthday"]
 
-def get_login(user_info,access_token,id_token):
-	f_param = get_f(access_token)
+def get_login(access_token,id_token):
+	f_param = get_f(id_token,1)
 	url = "https://api-lp1.znc.srv.nintendo.net/v3/Account/Login"
-	user_lang, user_country, user_birthday = user_info
+	user_lang, user_country, user_birthday = get_me(access_token)
 	body = {
 		"parameter" : {
 			"f" : f_param["f"],
@@ -75,25 +79,72 @@ def get_login(user_info,access_token,id_token):
 			}
 	}
 	headers = {
-		"X-ProductVersion" : "2.5.2",
-		"Content-Type" : "application/json; charset=utf-8",
+		"X-ProductVersion" : X_ProductVersion,
 		"Host" : "api-lp1.znc.srv.nintendo.net",
-		"X-Platform" : "iOS",
-		"Accept-Encoding": "gzip",
-		"Connection": "Keep-Alive",
-		"User-Agent": "Coral/2.5.2 (com.nintendo.znca; iOS 16.5)"
+		"X-Platform" : "iOS"
 	}
 	response = requests.post(url, headers=headers, json=body)
-	return response.text
+	return json.loads(response.text)["result"]["webApiServerCredential"]["accessToken"]
+
+def get_gtoken(access_token):
+	f_param = get_f(access_token,2)
+	url = "https://api-lp1.znc.srv.nintendo.net/v2/Game/GetWebServiceToken"
+	body = {
+		"parameter" : {
+			"f" : f_param["f"],
+			"timestamp" : f_param["timestmp"],
+			"requestId" : f_param["requestId"],
+			"registrationToken" : access_token,
+			"id" :  4834290508791808
+			}
+	}
+	headers = {
+		"X-Platform" : "iOS",
+		"X-ProductVersion" : X_ProductVersion,
+		"Authorization" : f"Bearer {access_token}"
+	}
+	response = requests.post(url, headers=headers, json=body)
+	return json.loads(response.text)["result"]["accessToken"]
+
+def get_bulllet_token(gtoken):
+	url = "https://api.lp1.av5ja.srv.nintendo.net/api/bullet_tokens"
+	headers = {
+		"X-Web-View-Ver" : X_Web_View_Ver
+	}
+	cookies = {
+		"_gtoken": gtoken,
+	}
+	response = requests.post(url, headers=headers, cookies=cookies)
+	return json.loads(response.text)["bulletToken"]
+
+def api_test(gtokne,bullet_token):
+	url = "https://api.lp1.av5ja.srv.nintendo.net/api/graphql"
+	body = {
+		"variables": {},
+		"extensions": {
+			"persistedQuery": {
+				"version": 1,
+				"sha256Hash": "0d90c7576f1916469b2ae69f64292c02"
+			}
+		}
+	}
+	headers = {
+		"Authorization" : f"Bearer {bullet_token}",
+		"X-Web-View-Ver" : X_Web_View_Ver
+
+	}
+	cookies = {
+		"_gtoken": gtokne,
+	}
+	response = requests.post(url, headers=headers, cookies=cookies, json=body)
+	return json.loads(response.text)
+
 
 if __name__ == "__main__":
-	access_token, id_token = get_access_token(get_session_token())
-	user_lang, user_country, user_birthday = get_me(access_token)
+	X_Web_View_Ver = "4.0.0-d5178440"
+	X_ProductVersion = "2.5.2"
+	access_token,id_token = get_access_token(get_session_token())
 	print("----------------------------")
-	# print(f"f : {f}")
-	# print(f"language : {user_lang}")
-	# print(f"naBirthday : {user_birthday}")
-	# print(f"naCountry : {user_country}")
-	# print(f"naIdToken : {id_token}")
-	# print("----------------------------")
-	print(get_login(get_me(access_token),access_token,id_token))
+	gtoken = get_gtoken(get_login(access_token,id_token))
+	bullet_token = get_bulllet_token(gtoken)
+	print(api_test(gtoken,bullet_token))
